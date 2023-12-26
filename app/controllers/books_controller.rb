6bb2479cb89164b499046ac67a416ac303a7f6d4
3369yu <151356820+3369yu@users.noreply.github.com>
2023-12-26 07:27:42 +0000

    [Finish] bookのshowページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,36 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def index
+    @book = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+  
+  def destory
+    book = Book.find(params[:id])
+    book.destory
+    redirect_to '/books'
+  end
+
+   private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
